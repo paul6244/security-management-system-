d9ef0572cea0f78d0b3c96eb5e0c1fbe6cf238a1
paul6244 <paulglawu6@gmail.com>
2026-03-27 06:26:39 +0000

    Fix getAllPersonnel: get email from users table instead of security_personnel table

--- src/java/model/Mymodel.java
+++ src/java/model/Mymodel.java
@@ -254,10 +254,10 @@ public class Mymodel {
     public static ResultSet getAllPersonnel() {
         try {
             Connection conn = DatabaseConfig.getConnection();
-            String sql = "SELECT sp.id, sp.name, sp.shift_time, sp.email, b.name as branch_name, u.username " +
+            String sql = "SELECT sp.id, sp.name, sp.shift_time, u.email, b.name as branch_name, u.username " +
                         "FROM security_personnel sp " +
                         "JOIN branches b ON sp.branch_id = b.id " +
-                        "JOIN users u ON sp.user_id = u.id";
+                        "LEFT JOIN users u ON sp.user_id = u.id";
             PreparedStatement ps = conn.prepareStatement(sql);
             return ps.executeQuery();
         } catch (Exception e) {
