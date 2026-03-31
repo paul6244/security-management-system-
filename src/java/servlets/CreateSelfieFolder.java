2c0c1d5f5bb7c95d72b8094576bfc3c338cfde56
paul6244 <paulglawu6@gmail.com>
2026-03-31 07:48:51 +0000

    Add @WebServlet annotations to CreateSelfieFolder and SystemDiagnostics

--- src/java/servlets/CreateSelfieFolder.java
+++ src/java/servlets/CreateSelfieFolder.java
@@ -3,10 +3,12 @@ package servlets;
 import java.io.File;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/CreateSelfieFolder")
 public class CreateSelfieFolder extends HttpServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
--- src/java/servlets/SystemDiagnostics.java
+++ src/java/servlets/SystemDiagnostics.java
@@ -7,11 +7,13 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import config.SimpleDatabaseConfig;
 
+@WebServlet("/SystemDiagnostics")
 public class SystemDiagnostics extends HttpServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
