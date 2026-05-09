18c217c9df4a23582cd4d48929c611a7684f2768
paul6244 <paulglawu6@gmail.com>
2026-05-09 21:40:28 +0000

    Fix missing HttpServlet import in AddChecklistItem
    - Add missing HttpServlet import to resolve compilation error
    - Remove duplicate AddChecklistItemFixed file
    - Ensure clean compilation without errors

--- src/java/servlets/AddChecklistItem.java
+++ src/java/servlets/AddChecklistItem.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
 import com.google.gson.Gson;
 import config.DatabaseConfig;
 
